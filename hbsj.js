140953b5d2dc6998a4f2cb0c742ba5f221aef341
ruiqicwbjzx <52522409+ruiqicwbjzx@users.noreply.github.com>
2024-04-02 17:27:28 +0800

    Update hbsj.js

--- hbsj.js
+++ hbsj.js
@@ -1,3 +1,12 @@
+
+//
+[rewrite_local]
+^https:\/\/poster\.leminet\.cn\/v01\/profile url script-response-body hbsj.js
+[mitm] 
+hostname = poster.leminet.cn
+//
+
+
 var body = $response.body;
 body = body.replace(/"vip":\w+/g, '"vip":true');
 body = body.replace(/"vip_expire":\d+/g, '"vip_expire":32480034939');
